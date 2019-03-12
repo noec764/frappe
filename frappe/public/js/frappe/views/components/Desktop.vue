911de5e5b049be4e745192a9e1db395b4f69c653
Charles-Henri Decultot <chdecultot@dokos.io>
2019-03-12 15:22:17 +0100

    Fix: Frappe fixes

--- frappe/public/js/frappe/views/components/Desktop.vue
+++ frappe/public/js/frappe/views/components/Desktop.vue
@@ -8,6 +8,7 @@
 				v-if="modules.filter(m => m.category === category).length"
 				:category="category"
 				:all_modules="modules.filter(m => m.category === category)"
+				:customization_settings="home_settings ? home_settings[category] : {}"
 			>
 			</desk-section>
 
@@ -18,7 +19,6 @@
 
 <script>
 import DeskSection from './DeskSection.vue';
-
 export default {
 	components: {
 		DeskSection
@@ -26,23 +26,38 @@ export default {
 	data() {
 		let modules_list = frappe.boot.allowed_modules
 			.filter(d => (d.type==='module' || d.category==='Places') && !d.blocked);
-
 		modules_list.forEach(module => {
 			module.count = this.get_module_count(module.module_name);
 		});
-
 		return {
 			route_str: frappe.get_route()[1],
 			module_label: '',
 			module_categories: ["Modules", "Domains", "Places", "Administration"],
-			modules: modules_list
+			modules: modules_list,
+			// // Desk customizations. Format of user settings:
+			// home_settings = {				// <--- Settings
+			// 	"Domains": {   				// <--- Category (Desk Section)
+			// 		"Manufacturing": {		// <--- Module
+			// 			"index": 3,
+			// 			"links": [],
+			// 			"hidden": 1,
+			// 		},
+			// 	},
+			// }
+			home_settings: JSON.parse(frappe.boot.home_settings)
 		};
 	},
 	methods: {
+		get_settings() {
+			return frappe.db.get_value('User', user, 'home_settings')
+				.then(resp => {
+					this.all_settings = JSON.parse(resp.message['home_settings']);
+					this.settings = this.all_settings[this.category];
+				});
+		},
 		get_module_count(module_name) {
 			var module_doctypes = frappe.boot.notification_info.module_doctypes[module_name];
 			var sum = 0;
-
 			if(module_doctypes && frappe.boot.notification_info.open_count_doctype) {
 				// sum all doctypes for a module
 				for (var j=0, k=module_doctypes.length; j < k; j++) {
@@ -52,7 +67,6 @@ export default {
 					sum += count;
 				}
 			}
-
 			if(frappe.boot.notification_info.open_count_doctype
 				&& frappe.boot.notification_info.open_count_doctype[module_name]!=null) {
 				// notification count explicitly for doctype
@@ -60,7 +74,6 @@ export default {
 				count = typeof count == "string" ? parseInt(count) : count;
 				sum += count;
 			}
-
 			if(frappe.boot.notification_info.open_count_module
 				&& frappe.boot.notification_info.open_count_module[module_name]!=null) {
 				// notification count explicitly for module
@@ -68,9 +81,7 @@ export default {
 				count = typeof count == "string" ? parseInt(count) : count;
 				sum += count;
 			}
-
 			sum = sum > 99 ? "99+" : sum;
-
 			return sum;
 		}
 	}
@@ -81,10 +92,7 @@ export default {
 .modules-page-container {
 	margin-bottom: 30px;
 }
-
 .toolbar-underlay{
 	margin: 70px;
 }
-
 </style>
-
