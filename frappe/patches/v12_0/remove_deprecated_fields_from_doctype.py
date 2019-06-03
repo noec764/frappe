a976f94c7d2aedcceef7024089079f1f6612b8f1
Charles-Henri Decultot <chdecultot@dokos.io>
2019-06-03 19:31:53 +0200

    fix: sql issue in patch

--- frappe/patches/v12_0/remove_deprecated_fields_from_doctype.py
+++ frappe/patches/v12_0/remove_deprecated_fields_from_doctype.py
@@ -2,6 +2,8 @@ import frappe
 
 def execute():
 	frappe.reload_doc('core', 'doctype', 'doctype')
+	frappe.db.commit()
+
 	frappe.model.delete_fields({
 		'DocType': ['hide_heading', 'image_view', 'read_only_onload']
 	}, delete=1)
