b4fdd5ecee7ea071d5200b75282aaa559534c823
Charles-Henri Decultot <chdecultot@dokos.io>
2019-10-09 12:01:25 +0200

    fix: New status for not handled events

--- frappe/integrations/doctype/integration_request/integration_request.js
+++ frappe/integrations/doctype/integration_request/integration_request.js
@@ -13,19 +13,19 @@ frappe.ui.form.on('Integration Request', {
 					})
 				}
 			);
-		}
 
-		if(frm.doc.integration_type === 'Webhook' && (frm.doc.status !== 'Completed' || frm.doc.error == 'This type of event is not handled by dokos')) {
-			frm.page.add_action_item(
-				__('Retry processing the webhook'),
-				() => {
-					frappe.call({method: "retry_webhook", doc: frm.doc})
-					.then(r => {
-						frappe.show_alert({message:__("Processing in progress"), indicator:'green'});
-						frm.reload_doc()
-					})
-				}
-			);
+			if(frm.doc.integration_type === 'Webhook') {
+				frm.page.add_action_item(
+					__('Retry processing the webhook'),
+					() => {
+						frappe.call({method: "retry_webhook", doc: frm.doc})
+						.then(r => {
+							frappe.show_alert({message:__("Processing in progress"), indicator:'green'});
+							frm.reload_doc()
+						})
+					}
+				);
+			}
 		}
 
 	}
