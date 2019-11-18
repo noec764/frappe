0682cfad79ccf07dfdabfcadae83a92a7cc7b946
Charles-Henri Decultot <chdecultot@dokos.io>
2019-11-18 15:55:25 +0100

    feat: Add RRULE library

---
+++ frappe/public/js/lib/rrule/locales/fr.js
@@ -0,0 +1,46 @@
+const RRULE_DATES = {
+	dayNames: [
+		'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
+		'Jeudi', 'Vendredi', 'Samedi'
+	],
+	monthNames: [
+		'Janvier', 'Février', 'Mars', 'Avril', 'Mai',
+		'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre',
+		'Novembre', 'Décembre'
+	]
+}
+
+const RRULE_STRINGS = {
+	every: 'tous les',
+	until: 'jusqu\'au',
+	day: 'jour',
+	days: 'jours',
+	week: 'semaine',
+	weeks: 'semaines',
+	on: 'le',
+	in: 'in',
+	'on the': 'le',
+	for: 'pour',
+	and: 'et',
+	or: 'ou',
+	at: 'à',
+	last: 'dernier',
+	'(~ approximate)': '(~ approx.)',
+	times: 'temps',
+	time: 'temps',
+	minutes: 'minutes',
+	minute: 'minute',
+	hours: 'heures',
+	hour: 'heure',
+	weekdays: 'jours de la semaine',
+	weekday: 'jour de la semaine',
+	months: 'mois',
+	month: 'mois',
+	years: 'années',
+	year: 'année',
+	rd: ' ',
+	the: 'le',
+	th: ' ',
+	st: ' ',
+	nd: ' '
+}
