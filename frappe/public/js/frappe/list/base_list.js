471bbd4ac020c5d21c7dda78edd89d401671729f
Charles-Henri Decultot <chdecultot@dokos.io>
2019-12-05 14:54:46 +0100

    fix: remove default values in standard filters

--- frappe/public/js/frappe/list/base_list.js
+++ frappe/public/js/frappe/list/base_list.js
@@ -625,17 +625,13 @@ class FilterArea {
 					options = options.join("\n");
 				}
 			}
-			let default_value = (fieldtype === 'Link') ? frappe.defaults.get_user_default(options) : null;
-			if (['__default', '__global'].includes(default_value)) {
-				default_value = null;
-			}
+
 			return {
 				fieldtype: fieldtype,
 				label: __(df.label),
 				options: options,
 				fieldname: df.fieldname,
 				condition: condition,
-				default: default_value,
 				onchange: () => this.refresh_list_view(),
 				ignore_link_validation: fieldtype === 'Dynamic Link'
 			};
