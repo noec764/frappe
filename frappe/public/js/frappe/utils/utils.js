2b3ede1e0f5433158265137bfee0e23eba752c6a
Charles-Henri Decultot <chdecultot@dokos.io>
2019-12-24 10:00:12 +0100

    fix: Translations

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -656,16 +656,20 @@ Object.assign(frappe.utils, {
 		let route = route_str.split('/');
 
 		if (route[2] === 'Report' || route[0] === 'query-report') {
-			return __('{0} Report', [route[3] || route[1]]);
+			const doctype = __(route[3] || route[1])
+			return __("{0} Report", [doctype]);
 		}
 		if (route[0] === 'List') {
-			return __('{0} List', [route[1]]);
+			const doctype = __(route[1])
+			return __("{0} List", [doctype]);
 		}
 		if (route[0] === 'modules') {
-			return __('{0} Modules', [route[1]]);
+			const doctype = __(route[1])
+			return __("{0} Modules", [doctype]);
 		}
 		if (route[0] === 'dashboard') {
-			return __('{0} Dashboard', [route[1]]);
+			const doctype = __(route[1])
+			return __("{0} Dashboard", [doctype]);
 		}
 		return __(frappe.utils.to_title_case(route[0], true));
 	},
