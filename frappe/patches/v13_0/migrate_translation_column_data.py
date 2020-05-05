0ed55abd3a156d3554e84426b1073818598f6ba5
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-05 11:15:34 +0200

    fix: UI fixes

--- frappe/patches/v13_0/migrate_translation_column_data.py
+++ frappe/patches/v13_0/migrate_translation_column_data.py
@@ -3,7 +3,7 @@ from frappe.model.utils.rename_field import rename_field
 from frappe.model.meta import get_table_columns
 
 def execute():
-	frappe.reload_doc('desk', 'doctype', 'Translation')
+	frappe.reload_doc('core', 'doctype', 'Translation')
 	
 	if "source_name" in get_table_columns("Translation"):
 		rename_field("Translation", "source_name", "source_text")
