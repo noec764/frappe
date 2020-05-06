ca442f02c00b2b67bf4fe7ab95c1bbb29bafc10d
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-06 09:53:44 +0200

    feat: Flush old background jobs logs by default

--- frappe/core/doctype/scheduled_job_log/scheduled_job_log.py
+++ frappe/core/doctype/scheduled_job_log/scheduled_job_log.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe.utils import cint
 from frappe.model.document import Document
 
 class ScheduledJobLog(Document):
 	pass
+
+def flush():
+	if cint(frappe.get_system_settings("restricted_jobs_logs")):
+		frappe.db.sql("""
+			DELETE FROM `tabScheduled Job Log`
+			WHERE datediff(curdate(), creation) > 30
+		""")
