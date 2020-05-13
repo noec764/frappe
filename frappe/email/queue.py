73b9c93acf35506b1ab0dae8411e36cefac07011
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-13 07:28:54 +0200

    fix: Check for message content before adding it to sent folder

--- frappe/email/queue.py
+++ frappe/email/queue.py
@@ -470,7 +470,7 @@ def send_one(email, smtpserver=None, auto_commit=True, now=False, from_test=Fals
 		if email.communication:
 			frappe.get_doc('Communication', email.communication).set_delivery_status(commit=auto_commit)
 
-		if smtpserver.append_emails_to_sent_folder and email_sent_to_any_recipient:
+		if message and smtpserver.append_emails_to_sent_folder and email_sent_to_any_recipient:
 			smtpserver.email_account.append_email_to_sent_folder(encode(message))
 
 	except (smtplib.SMTPServerDisconnected,
