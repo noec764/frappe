199ab248fb53843e454f486b88a7974d2889e359
Charles-Henri Decultot <chdecultot@dokos.io>
2020-05-28 08:20:02 +0200

    fix: trigger after submit route hook after on_submit

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -647,13 +647,15 @@ frappe.ui.form.Form = class FrappeForm {
 							frappe.utils.play_sound("submit");
 							callback && callback();
 							me.script_manager.trigger("on_submit")
-								.then(() => resolve(me));
-							if (frappe.route_hooks.after_submit) {
-								let route_callback = frappe.route_hooks.after_submit;
-								delete frappe.route_hooks.after_submit;
+								.then(() => resolve(me))
+								.then(() => {
+									if (frappe.route_hooks.after_submit) {
+										let route_callback = frappe.route_hooks.after_submit;
+										delete frappe.route_hooks.after_submit;
 
-								route_callback(me);
-							}
+										route_callback(me);
+									}
+								});
 						}
 					}, btn, () => me.handle_save_fail(btn, on_error), resolve);
 				});
