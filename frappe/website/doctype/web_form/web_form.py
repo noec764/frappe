c7ad00a11bbc30e7783aa09c6b74f55968211f7d
Charles-Henri Decultot <chdecultot@dokos.io>
2020-07-09 08:50:29 +0200

    fix: limit commenting to logged in accounts

--- frappe/website/doctype/web_form/web_form.py
+++ frappe/website/doctype/web_form/web_form.py
@@ -122,10 +122,6 @@ def get_context(context):
 		'''Build context to render the `web_form.html` template'''
 		self.set_web_form_module()
 
-		context._login_required = False
-		if self.login_required and frappe.session.user == "Guest":
-			context._login_required = True
-
 		doc, delimeter = make_route_string(frappe.form_dict)
 		context.doc = doc
 		context.delimeter = delimeter
@@ -143,7 +139,7 @@ def get_context(context):
 			self.use_meta_fields()
 
 		context.allow_edit = self.allow_edit
-		if not context._login_required:
+		if not frappe.session.user == "Guest":
 			if self.allow_multiple:
 				if not frappe.form_dict.name and not frappe.form_dict.new:
 					# list data is queried via JS
