4d12948fcf8dbe51f9939366a4e86d597ff95b46
Charles-Henri Decultot <chdecultot@dokos.io>
2020-08-04 09:23:49 +0200

    fix: whitelist functions called through search widget

--- frappe/email/__init__.py
+++ frappe/email/__init__.py
@@ -59,6 +59,7 @@ def relink(name, reference_doctype=None, reference_name=None):
 			communication_type = "Communication" and
 			name = %s""", (reference_doctype, reference_name, name))
 
+@frappe.whitelist()
 def get_communication_doctype(doctype, txt, searchfield, start, page_len, filters):
 	from frappe.modules import load_doctype_module
 	com_doctypes = []
