62697ac06ef12149e6d1419330ef2b840e6bd336
Charles-Henri Decultot <chdecultot@dokos.io>
2020-08-28 12:26:05 +0200

    fix: leaderboard style

--- frappe/desk/page/leaderboard/leaderboard.js
+++ frappe/desk/page/leaderboard/leaderboard.js
@@ -14,11 +14,12 @@ class Leaderboard {
 		frappe.ui.make_app_page({
 			parent: parent,
 			title: __("Leaderboard"),
-			single_column: false
+			single_column: false,
+			card_layout: true,
 		});
 		this.parent = parent;
 		this.page = this.parent.page;
-		this.page.sidebar.html(`<ul class="module-sidebar-nav overlay-sidebar nav flex-column"></ul>`);
+		this.page.sidebar.html(`<ul class="desk-sidebar leaderboard-sidebar overlay-sidebar"></ul>`);
 		this.$sidebar_list = this.page.sidebar.find('ul');
 
 		this.get_leaderboard_config();
@@ -142,7 +143,7 @@ class Leaderboard {
 				fieldname: 'selected_date_range',
 				placeholder: "Date Range",
 				default: [frappe.datetime.month_start(), frappe.datetime.now_date()],
-				input_class: 'input-sm',
+				input_class: 'input-xs',
 				reqd: 1,
 				change: () => {
 					this.selected_date_range = date_field.get_value();
@@ -188,7 +189,7 @@ class Leaderboard {
 
 		this.$search_box =
 			$(`<div class="leaderboard-search form-group col-md-3">
-				<input type="text" placeholder="Search" data-element="search" class="form-control leaderboard-search-input input-sm">
+				<input type="text" placeholder="Search" data-element="search" class="form-control leaderboard-search-input input-xs">
 			</div>`);
 
 		$(this.parent).find(".page-form").append(this.$search_box);
@@ -301,7 +302,7 @@ class Leaderboard {
 
 		const html =
 			`<div class="list-headers">
-				<div class="list-item list-item--head" data-list-renderer="List">${filters}</div>
+				<div class="list-item" data-list-renderer="List">${filters}</div>
 			</div>`;
 		return html;
 	}
@@ -364,7 +365,7 @@ class Leaderboard {
 	}
 
 	get_sidebar_item(item) {
-		return $(`<li class="strong nav-item leaderboard-item">
+		return $(`<li class="desk-sidebar-item">
 			<a class="nav-link">
 			<span doctype-value="${item}">${ __(item) }</span></a>
 		</li>`);
