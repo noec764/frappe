f791d2718dc0649d8347a3f0ad6ba12ebacde714
Charles-Henri Decultot <chdecultot@dokos.io>
2020-09-01 09:15:56 +0200

    fix: Add empty state for events

--- frappe/public/js/frappe/ui/notifications/notifications.js
+++ frappe/public/js/frappe/ui/notifications/notifications.js
@@ -468,9 +468,12 @@ class EventsView extends BaseNotificationsView {
 			};
 			html = event_list.map(get_event_html).join('');
 		} else {
-			html = `<li class="recent-item text-center">
-					<span class="text-muted">${__('No Planned Events')}</span>
-				</li>`;
+			html = `
+				<div class="empty-state center-content">
+					<img src="/assets/frappe/images/ui-states/event-empty-state.svg"></img>
+					${__('No Events Today')}
+				</div>
+			`;
 		}
 
 		this.container.html(html);
