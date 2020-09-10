ac1a4aad8e1b989136cff5c9f741e1bd7d9b419f
Charles-Henri Decultot <chdecultot@dokos.io>
2020-09-10 18:50:18 +0200

    fix: retest fixes

--- frappe/core/doctype/payment_gateway/payment_gateway_dashboard.py
+++ frappe/core/doctype/payment_gateway/payment_gateway_dashboard.py
@@ -3,10 +3,5 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname': 'payment_gateway',
-		'transactions': [
-			{
-				'label': _('Accounts'),
-				'items': ['Payment Gateway Account']
-			}
-		]
+		'transactions': []
 	}
