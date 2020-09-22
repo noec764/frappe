6dfca02038cb2de74e0b371b29b7a9933e94c220
Charles-Henri Decultot <chdecultot@dokos.io>
2020-09-22 18:48:00 +0200

    fix: force reload doctype

--- frappe/patches/v13_0/set_route_for_blog_category.py
+++ frappe/patches/v13_0/set_route_for_blog_category.py
@@ -1,7 +1,7 @@
 import frappe
 
 def execute():
-	frappe.reload_doc("core", "doctype", "DocType")
+	frappe.reload_doc("core", "doctype", "DocType", force=True)
 	categories = frappe.get_list("Blog Category")
 	for category in categories:
 		doc = frappe.get_doc("Blog Category", category["name"])
