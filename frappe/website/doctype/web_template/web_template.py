cfb2d7bf0d27a599376ce12f0d7d3a3c3c89e6a2
Charles-Henri Decultot <chdecultot@dokos.io>
2020-10-06 14:53:05 +0200

    fix(web-template): handle None values

--- frappe/website/doctype/web_template/web_template.py
+++ frappe/website/doctype/web_template/web_template.py
@@ -80,7 +80,10 @@ class WebTemplate(Document):
 
 		return template
 
-	def render(self, values="{}"):
+	def render(self, values=None):
+		if not values:
+			values = {}
+
 		values = frappe.parse_json(values)
 		values.update({"values": values})
 		template = self.get_template(self.standard)
