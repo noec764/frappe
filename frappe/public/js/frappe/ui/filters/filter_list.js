39733b6ddc7e1b3a981917c9250d24bd54d392f4
Charles-Henri Decultot <chdecultot@dokos.io>
2020-10-20 14:34:50 +0200

    fix: filter apply and remove

--- frappe/public/js/frappe/ui/filters/filter_list.js
+++ frappe/public/js/frappe/ui/filters/filter_list.js
@@ -76,8 +76,7 @@ frappe.ui.FilterGroup = class {
 		});
 
 		this.filter_button.on('hidden.bs.popover', (e) => {
-			this.update_filters();
-			this.on_change();
+			this.apply();
 		});
 
 		// REDESIGN-TODO: (Temporary) Review and find best solution for this
@@ -94,6 +93,11 @@ frappe.ui.FilterGroup = class {
 			filter.field = null;
 			filter.make();
 		});
+	}
+
+	apply() {
+		this.update_filters();
+		this.on_change();
 	}
 
 	update_filter_button() {
@@ -128,8 +132,7 @@ frappe.ui.FilterGroup = class {
 
 		this.wrapper.find('.apply-filters').on('click', () => {
 			this.toggle_empty_filters(false);
-			this.update_filters();
-			this.on_change();
+			this.apply();
 		});
 	}
 
