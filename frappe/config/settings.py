a39f3f069baffffdd95ebe8e6b5940276c6885bd
Charles-Henri Decultot <chdecultot@dokos.io>
2020-10-21 10:46:29 +0200

    feat: Log settings

--- frappe/config/settings.py
+++ frappe/config/settings.py
@@ -17,6 +17,11 @@ def get_data():
 				},
 				{
 					"type": "doctype",
+					"name": "Log Settings",
+					"description": _("Log cleanup and notification configuration")
+				},
+				{
+					"type": "doctype",
 					"name": "Error Log",
 					"description": _("Log of error on automated events (scheduler).")
 				},
--- frappe/tests/test_email.py
+++ frappe/tests/test_email.py
@@ -137,8 +137,10 @@ class TestEmail(unittest.TestCase):
 		frappe.flags.mute_emails = False
 		self.test_email_queue()
 		frappe.db.sql("UPDATE `tabEmail Queue` SET `modified`=(NOW() - INTERVAL '8' day)")
-		from frappe.email.queue import clear_outbox
-		clear_outbox()
+
+		from frappe.email.queue import set_expiry_for_email_queue
+		set_expiry_for_email_queue()
+
 		email_queue = frappe.db.sql("""select name from `tabEmail Queue` where status='Expired'""", as_dict=1)
 		self.assertEqual(len(email_queue), 1)
 		queue_recipients = [r.recipient for r in frappe.db.sql("""select recipient from `tabEmail Queue Recipient`
