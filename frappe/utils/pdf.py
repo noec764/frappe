000e9cc4b0380b940fbc687e3e3dc94eb01c7d31
Charles-Henri Decultot <chdecultot@dokos.io>
2020-10-28 15:10:10 +0100

    fix(pdf): PDF generation shouldn't fail in background jobs and tests

--- frappe/utils/pdf.py
+++ frappe/utils/pdf.py
@@ -115,8 +115,7 @@ def prepare_options(html, options):
 	options.update(html_options or {})
 
 	# cookies
-	if not frappe.flags.in_test:
-		options.update(get_cookie_options())
+	options.update(get_cookie_options())
 
 	# page size
 	if not options.get("page-size"):
@@ -134,7 +133,7 @@ def get_cookie_options():
 		# https://werkzeug.palletsprojects.com/en/0.16.x/wrappers/#werkzeug.wrappers.BaseRequest.host
 		domain = ""
 		if hasattr(frappe.local, 'request'):
-			domain = frappe.local.request.host.split(":", 1)[0]
+			domain = frappe.utils.get_host_name().split(":", 1)[0]
 		with open(cookiejar, "w") as f:
 			f.write("sid={}; Domain={};\n".format(frappe.session.sid, domain))
 
