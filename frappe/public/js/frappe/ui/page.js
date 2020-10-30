b355da6e77644dd4294103fb11ac7ee3441236c3
Charles-Henri Decultot <chdecultot@dokos.io>
2020-10-30 08:36:02 +0100

    fix: Remove unwanted icon-btn class

--- frappe/public/js/frappe/ui/page.js
+++ frappe/public/js/frappe/ui/page.js
@@ -659,7 +659,7 @@ frappe.ui.Page = Class.extend({
 
 		let custom_btn_group = $(`
 			<div class="custom-btn-group hide">
-				<button type="button" class="btn btn-default btn-sm ${icon ? "icon-btn" : "ellipsis"}" data-toggle="dropdown" aria-expanded="false">
+				<button type="button" class="btn btn-default btn-sm ellipsis" data-toggle="dropdown" aria-expanded="false">
 					${dropdown_label}
 				</button>
 				<ul class="dropdown-menu dropdown-menu-right" role="menu"></ul>
