3915bf4ce864b948c1bf6059a8829d4c1485b837
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-02 10:01:22 +0100

    fix: desk shortcut patch

--- frappe/patches/dokos/v2_0/correct_desk_shortcuts_color.py
+++ frappe/patches/dokos/v2_0/correct_desk_shortcuts_color.py
@@ -1,5 +1,6 @@
 import frappe
 
 def execute():
+	frappe.reload_doc("desk", "doctype", "Desk Shortcut")
 	for shortcut in frappe.get_all("Desk Shortcut", filters={"color": "Grey"}):
 		frappe.db.set_value("Desk Shortcut", shortcut.name, "color", "Gray")
