bf37d8891d365eefd30964ad9092d3aa8989dd14
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-08 08:07:53 +0100

    refactor: move utils from widget

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -1113,6 +1113,30 @@ Object.assign(frappe.utils, {
 		return `/app/${route}`;
 	},
 
+	build_summary_item(summary) {
+		if (summary.type == "separator") {
+			return $(`<div class="summary-separator">
+				<div class="summary-value ${summary.color ? summary.color.toLowerCase() : 'text-muted'}">${summary.value}</div>
+			</div>`);
+		}
+		let df = { fieldtype: summary.datatype };
+		let doc = null;
+
+		if (summary.datatype == "Currency") {
+			df.options = "currency";
+			doc = { currency: summary.currency };
+		}
+
+		let value = frappe.format(summary.value, df, { only_value: true }, doc);
+		let color = summary.indicator ? summary.indicator.toLowerCase()
+			: summary.color ? summary.color.toLowerCase() : '';
+
+		return $(`<div class="summary-item">
+			<span class="summary-label">${summary.label}</span>
+			<div class="summary-value ${color}">${value}</div>
+		</div>`);
+	},
+
 	shorten_number: function (number, country, min_length=4, max_no_of_decimals=2) {
 		/* returns the number as an abbreviated string
 		 * PARAMS
