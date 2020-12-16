9ad909a46c68d4abd9250d0cf2b46c741abdefca
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-16 15:21:39 +0100

    feat: update tooltip delay

--- frappe/public/js/frappe/ui/iconbar.js
+++ frappe/public/js/frappe/ui/iconbar.js
@@ -31,9 +31,8 @@ frappe.ui.IconBar = Class.extend({
 				return false;
 			});
 			
-		$li.find("i").attr("title", label).tooltip();
+		$li.find("i").attr("title", label).tooltip({ delay: { "show": 600, "hide": 100 } });
 
-			
 		this.$wrapper.find(".iconbar-" + group).removeClass("hide")
 		this.show();
 		return $li;
