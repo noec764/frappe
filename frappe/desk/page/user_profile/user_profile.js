793b403efca8ff052891eb338221dd7581cfb275
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-18 18:13:02 +0100

    fix: Bind user profile show to page show

--- frappe/desk/page/user_profile/user_profile.js
+++ frappe/desk/page/user_profile/user_profile.js
@@ -1,10 +1,13 @@
 frappe.pages['user-profile'].on_page_load = function (wrapper) {
+	frappe.ui.make_app_page({
+		parent: wrapper,
+	});
 
 	frappe.require('assets/js/user_profile_controller.min.js', () => {
-		frappe.ui.make_app_page({
-			parent: wrapper,
-		});
 		let user_profile = new frappe.ui.UserProfile(wrapper);
 		user_profile.show();
+		$(wrapper).bind('show', () => {
+			user_profile.show();
+		});
 	});
 };
