392c27b83f45b455bb3240dda00017a2216d0a89
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-30 10:16:03 +0100

    chore: french translations

--- frappe/automation/doctype/auto_repeat/auto_repeat.py
+++ frappe/automation/doctype/auto_repeat/auto_repeat.py
@@ -79,7 +79,7 @@ class AutoRepeat(Document):
 	def validate_submit_after_creation(self):
 		if self.submit_on_creation and not frappe.get_meta(self.reference_doctype).is_submittable:
 			frappe.throw(_('Cannot enable {0} for a non-submittable doctype').format(
-				frappe.bold('Submit on Creation')))
+				frappe.bold(_('Submit on Creation'))))
 
 	def validate_dates(self):
 		if frappe.flags.in_patch:
