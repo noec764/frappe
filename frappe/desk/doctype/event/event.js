e939ccfe859e978971d40c324834644d22fe505b
Charles-Henri Decultot <chdecultot@dokos.io>
2020-12-31 18:33:42 +0100

    feat: Item bookings in dashboards

--- frappe/desk/doctype/event/event.js
+++ frappe/desk/doctype/event/event.js
@@ -20,6 +20,7 @@ frappe.ui.form.on("Event", {
 	},
 	refresh: function(frm) {
 		frm.trigger('add_repeat_text')
+		frm.trigger('add_item_booking_details')
 	},
 	repeat_this_event: function(frm) {
 		if(frm.doc.repeat_this_event === 1) {
@@ -34,6 +35,35 @@ frappe.ui.form.on("Event", {
 	sync_with_google_calendar(frm) {
 		frappe.db.get_value("Google Calendar", {user: frappe.session.user}, "name", r => {
 			r&&r.name&&frm.set_value("google_calendar", r.name)
+		})
+	},
+	add_item_booking_details(frm) {
+		frappe.model.with_doctype("Item Booking", () => {
+			frappe.db.get_list('Item Booking', {filters: {event: frm.doc.name}, fields: ["name", "item_name", "color", "starts_on", "status"]}).then(data => {
+				if (data.length) {
+					const item_booking_section = data.map(d => {
+						const $card = $(`
+							<div class="item-booking-card" style="background-color: ${d.color || "var(--primary)"}">
+								<div class="flex align-items-center">
+									<div class="left-title" style="color: ${frappe.ui.color.get_contrast_color(d.color)}">${frappe.datetime.obj_to_user(d.starts_on).replace(new RegExp('[^\.]?' + moment(d.starts_on).format('YYYY') + '.?'), '')}</div>
+									<div class="right-body">
+										<div class="text-muted">${d.item_name}</div>
+										<div class="text-color small">${__(d.status)}</div>
+									</div>
+								</div>
+							</div>
+						`)
+	
+						$card.on("click", () => {
+							frappe.set_route("Form", "Item Booking", d.name)
+						})
+	
+						return $card
+					})
+	
+					frm.dashboard.add_section(item_booking_section).removeClass("form-dashboard-section")
+				}
+			})
 		})
 	}
 });
