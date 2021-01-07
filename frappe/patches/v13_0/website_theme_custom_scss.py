5567db81d669cd48a3a5e0e851886132523b5300
Charles-Henri Decultot <chdecultot@dokos.io>
2021-01-07 09:55:37 +0100

    fix: Migration patch

--- frappe/patches/v13_0/website_theme_custom_scss.py
+++ frappe/patches/v13_0/website_theme_custom_scss.py
@@ -2,6 +2,7 @@ import frappe
 
 def execute():
 	frappe.reload_doctype('Website Theme')
+	frappe.reload_doc('website', 'doctype', 'Website Theme Ignore App')
 	for theme in frappe.get_all('Website Theme'):
 		doc = frappe.get_doc('Website Theme', theme.name)
 		if not doc.get('custom_scss') and doc.theme_scss:
