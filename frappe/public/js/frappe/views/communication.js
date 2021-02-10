2090b2f4a32bb186a974b50fdaccfb82eb7a2894
Charles-Henri Decultot <chdecultot@dokos.io>
2021-02-10 09:08:09 +0100

    fix: Design fixes

--- frappe/public/js/frappe/views/communication.js
+++ frappe/public/js/frappe/views/communication.js
@@ -109,7 +109,7 @@ frappe.views.CommunicationComposer = Class.extend({
 				label: __("Send me a copy"),
 				fieldtype: "Check",
 				fieldname: "send_me_a_copy",
-				'default': frappe.boot.user.send_me_a_copy
+				default: frappe.boot.user.send_me_a_copy
 			},
 			{
 				label: __("Send Read Receipt"),
