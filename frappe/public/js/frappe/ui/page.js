5ab51e1662e7808ae7ce504df907f7988e98375c
Charles-Henri Decultot <chdecultot@dokos.io>
2021-02-26 08:20:38 +0100

    feat: placeholder for Select fields

--- frappe/public/js/frappe/ui/page.js
+++ frappe/public/js/frappe/ui/page.js
@@ -775,6 +775,8 @@ frappe.ui.Page = Class.extend({
 			df.placeholder = df.label;
 		}
 
+		df.input_class = 'input-xs';
+
 		var f = frappe.ui.form.make_control({
 			df: df,
 			parent: parent || this.page_form,
@@ -796,7 +798,7 @@ frappe.ui.Page = Class.extend({
 		// hidden fields dont have $input
 		if (!f.$input) f.make_input();
 
-		f.$input.addClass("input-xs").attr("placeholder", __(df.label));
+		f.$input.attr("placeholder", __(df.label));
 
 		if(df.fieldtype==="Check") {
 			$(f.wrapper).find(":first-child")
