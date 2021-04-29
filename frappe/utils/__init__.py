14dbb3cfc2e2f1bc216c81800af9c78886fcf423
Charles-Henri Decultot <chdecultot@dokos.io>
2021-04-29 08:43:06 +0200

    refactor: fix py3 datatypes and remove references to six in oauth

--- frappe/utils/__init__.py
+++ frappe/utils/__init__.py
@@ -2,9 +2,6 @@
 # MIT License. See license.txt
 
 # util __init__.py
-
-from __future__ import print_function, unicode_literals
-
 import functools
 import hashlib
 import io
@@ -14,13 +11,12 @@ import re
 import sys
 import traceback
 import typing
-
 from email.header import decode_header, make_header
 from email.utils import formataddr, parseaddr
 from gzip import GzipFile
 from typing import Generator, Iterable
-
 from urllib.parse import quote, urlparse
+
 from werkzeug.test import Client
 
 import frappe
@@ -70,7 +66,7 @@ def get_formatted_email(user, mail=None):
 def extract_email_id(email):
 	"""fetch only the email part of the Email Address"""
 	email_id = parse_addr(email)[1]
-	if email_id and isinstance(email_id, str) and not isinstance(email_id, str):
+	if email_id and isinstance(email_id, bytes):
 		email_id = email_id.decode("utf-8", "ignore")
 	return email_id
 
@@ -367,14 +363,14 @@ def get_site_url(site):
 
 def encode_dict(d, encoding="utf-8"):
 	for key in d:
-		if isinstance(d[key], str) and isinstance(d[key], str):
+		if isinstance(d[key], str):
 			d[key] = d[key].encode(encoding)
 
 	return d
 
 def decode_dict(d, encoding="utf-8"):
 	for key in d:
-		if isinstance(d[key], str) and not isinstance(d[key], str):
+		if isinstance(d[key], bytes):
 			d[key] = d[key].decode(encoding, "ignore")
 
 	return d
