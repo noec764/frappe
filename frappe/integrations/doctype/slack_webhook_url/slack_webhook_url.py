939ac789c8aaf88449677629bde812cd4ac3228b
Charles-Henri Decultot <chdecultot@dokos.io>
2021-05-25 08:11:59 +0200

    feat(minor): option to disable button in slack message

--- frappe/integrations/doctype/slack_webhook_url/slack_webhook_url.py
+++ frappe/integrations/doctype/slack_webhook_url/slack_webhook_url.py
@@ -22,23 +22,27 @@ class SlackWebhookURL(Document):
 	pass
 
 def send_slack_message(webhook_url, message, reference_doctype, reference_name):
-	slack_url = frappe.db.get_value("Slack Webhook URL", webhook_url, "webhook_url")
-	doc_url = get_url_to_form(reference_doctype, reference_name)
-	attachments = [
-		{
+	data = {"text": message, "attachments": []}
+
+	slack_url, show_link = frappe.db.get_value(
+		"Slack Webhook URL", webhook_url, ["webhook_url", "show_document_link"]
+	)
+
+	if show_link:
+		doc_url = get_url_to_form(reference_doctype, reference_name)
+		link_to_doc = {
 			"fallback": _("See the document at {0}").format(doc_url),
 			"actions": [
 				{
 					"type": "button",
 					"text": _("Go to the document"),
 					"url": doc_url,
-					"style": "primary"
+					"style": "primary",
 				}
-			]
+			],
 		}
-	]
+		data["attachments"].append(link_to_doc)
 
-	data = {"text": message, "attachments": attachments}
 	r = requests.post(slack_url, data=json.dumps(data))
 
 
