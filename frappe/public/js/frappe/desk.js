67719c0ee7613198630d14de6e8e1e2d0982ac46
Charles-Henri <chdecultot@dokos.io>
2021-07-24 08:22:13 +0200

    feat: Auto generate RTL styles using rtlcss

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -61,8 +61,6 @@ frappe.Application = class Application {
 			}
 		});
 
-		this.set_rtl();
-
 		// page container
 		this.make_page_container();
 		this.set_route();
@@ -505,17 +503,6 @@ frappe.Application = class Application {
 				frappe.container.page.save_action();
 			}
 		}, 100);
-	}
-
-	set_rtl() {
-		if (frappe.utils.is_rtl()) {
-			var ls = document.createElement('link');
-			ls.rel="stylesheet";
-			ls.type = "text/css";
-			ls.href= frappe.assets.bundled_asset("frappe-rtl.bundle.css");
-			document.getElementsByTagName('head')[0].appendChild(ls);
-			$('body').addClass('frappe-rtl');
-		}
 	}
 
 	show_change_log() {
