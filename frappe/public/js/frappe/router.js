378e34262561bceb83394dc99c448b08f8e52656
Charles-Henri <chdecultot@dokos.io>
2021-08-03 10:50:28 +0200

    fix: Inbox View & Kanban View not loading

--- frappe/public/js/frappe/router.js
+++ frappe/public/js/frappe/router.js
@@ -176,10 +176,8 @@ frappe.router = {
 			}
 		} else {
 			standard_route = ['List', doctype_route.doctype, frappe.utils.to_title_case(route[2])];
-			if (route[3]) {
-				// calendar / kanban / dashboard / folder name
-				standard_route.push([...route].splice(3, route.length));
-			}
+			// calendar / kanban / dashboard / folder
+			if (route[3]) standard_route.push(...route.slice(3, route.length));
 		}
 		return standard_route;
 	},
@@ -252,7 +250,7 @@ frappe.router = {
 		// example 1: frappe.set_route('a', 'b', 'c');
 		// example 2: frappe.set_route(['a', 'b', 'c']);
 		// example 3: frappe.set_route('a/b/c');
-		let route = arguments;
+		let route = Array.from(arguments);
 
 		return new Promise(resolve => {
 			route = this.get_route_from_arguments(route);
@@ -304,7 +302,7 @@ frappe.router = {
 				new_route = [this.slug(route[1]), 'view', route[2].toLowerCase()];
 
 				// calendar / inbox / file folder
-				if (route[3]) new_route.push([...route].slice(3, route.length));
+				if (route[3]) new_route.push(...route.slice(3, route.length));
 			} else {
 				if ($.isPlainObject(route[2])) {
 					frappe.route_options = route[2];
