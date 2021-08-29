7a576400b15718dba267297afe556d84cf6d7676
Charles-Henri <chdecultot@dokos.io>
2021-08-29 08:26:49 +0200

    fix: Set correct master version

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -27,7 +27,7 @@ from .utils.lazy_loader import lazy_import
 # Lazy imports
 faker = lazy_import('faker')
 
-__version__ = '2.0.0-dev'
+__version__ = '2.0.15'
 __title__ = "Dodock Framework"
 
 local = Local()
