f86d402be791ecac0ceff692dbe09c53cead7e21
Charles-Henri <chdecultot@dokos.io>
2021-09-08 22:29:15 +0200

    fix: RTL support for text editor

--- frappe/public/js/frappe/form/controls/comment.js
+++ frappe/public/js/frappe/form/controls/comment.js
@@ -105,8 +105,10 @@ frappe.ui.form.ControlComment = class ControlComment extends frappe.ui.form.Cont
 		return [
 			['bold', 'italic', 'underline'],
 			['blockquote', 'code-block'],
+			[{ 'direction': "rtl" }],
 			['link', 'image'],
 			[{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+			[{ 'align': [] }],
 			['clean']
 		];
 	}
