9612d643e6732bffdaa8e850f56fab7d8579498c
Charles-Henri <chdecultot@dokos.io>
2021-09-15 23:10:24 +0200

    fix: Misc fixes

--- frappe/core/doctype/version/version.py
+++ frappe/core/doctype/version/version.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
 
-# License: MIT. See LICENSE
-
-
 import frappe, json
 
 from frappe.model.document import Document
