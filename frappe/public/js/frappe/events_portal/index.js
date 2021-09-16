8e6ff7fa711119df4c38b0ba833601140308048b
Charles-Henri <chdecultot@dokos.io>
2021-09-16 08:12:44 +0200

    fix: include bundles

--- frappe/public/js/frappe/events_portal/index.js
+++ frappe/public/js/frappe/events_portal/index.js
@@ -23,8 +23,8 @@ class EventsPortalView {
 
 	show() {
 		frappe.require([
-			'/assets/js/moment-bundle.min.js',
-			'/assets/js/control.min.js'
+			'libs.bundle.js',
+			'controls.bundle.js'
 		], () => {
 			this.build_calendar()
 		});
