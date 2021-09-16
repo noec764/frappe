49988f9119994f7dcc95c73c6c1376e57e07335a
Charles-Henri <chdecultot@dokos.io>
2021-09-16 07:56:32 +0200

    fix(UI): Clicking area around file view row opens the file

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -1170,6 +1170,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 			if (
 				$target.hasClass("filterable") ||
 				$target.hasClass("select-like") ||
+				$target.hasClass("file-select") ||
 				$target.hasClass("list-row-like") ||
 				$target.is(":checkbox")
 			) {
