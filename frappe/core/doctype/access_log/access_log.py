a1cc62e089443ac385b856d90520fa871a5d244f
Charles-Henri <chdecultot@dokos.io>
2021-09-17 09:17:14 +0200

    feat: Support for Column comparison in DatabaseQuery

--- frappe/core/doctype/access_log/access_log.py
+++ frappe/core/doctype/access_log/access_log.py
@@ -2,8 +2,8 @@
 # License: MIT. See LICENSE
 
 import frappe
+from tenacity import retry, retry_if_exception_type, stop_after_attempt
 from frappe.model.document import Document
-
 
 class AccessLog(Document):
 	pass
@@ -11,25 +11,40 @@ class AccessLog(Document):
 
 @frappe.whitelist()
 @frappe.write_only()
-def make_access_log(doctype=None, document=None, method=None, file_type=None,
-		report_name=None, filters=None, page=None, columns=None):
+@retry(
+	stop=stop_after_attempt(3), retry=retry_if_exception_type(frappe.DuplicateEntryError)
+)
+def make_access_log(
+	doctype=None,
+	document=None,
+	method=None,
+	file_type=None,
+	report_name=None,
+	filters=None,
+	page=None,
+	columns=None,
+):
 
 	user = frappe.session.user
+	in_request = frappe.request and frappe.request.method == "GET"
 
-	doc = frappe.get_doc({
-		'doctype': 'Access Log',
-		'user': user,
-		'export_from': doctype,
-		'reference_document': document,
-		'file_type': file_type,
-		'report_name': report_name,
-		'page': page,
-		'method': method,
-		'filters': frappe.utils.cstr(filters) if filters else None,
-		'columns': columns
-	})
+	doc = frappe.get_doc(
+		{
+			"doctype": "Access Log",
+			"user": user,
+			"export_from": doctype,
+			"reference_document": document,
+			"file_type": file_type,
+			"report_name": report_name,
+			"page": page,
+			"method": method,
+			"filters": frappe.utils.cstr(filters) if filters else None,
+			"columns": columns,
+		}
+	)
 	doc.insert(ignore_permissions=True)
 
 	# `frappe.db.commit` added because insert doesnt `commit` when called in GET requests like `printview`
-	if frappe.request and frappe.request.method == 'GET':
+	# dont commit in test mode
+	if not frappe.flags.in_test or in_request:
 		frappe.db.commit()
