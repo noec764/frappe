3cf5c38b874dd74c8462aa8d1cd6665727dc0e12
Charles-Henri <chdecultot@dokos.io>
2021-10-05 13:56:49 +0200

    fix: Dynamix link in quick entry dialog

--- frappe/public/js/frappe/form/controls/dynamic_link.js
+++ frappe/public/js/frappe/form/controls/dynamic_link.js
@@ -3,7 +3,7 @@ frappe.ui.form.ControlDynamicLink = frappe.ui.form.ControlLink.extend({
 		let options = '';
 		if (this.df.get_options) {
 			options = this.df.get_options();
-		} else if (this.docname==null && cur_dialog) {
+		} else if ((this.docname==null || this.doc.__islocal) && cur_dialog) {
 			//for dialog box
 			options = cur_dialog.get_value(this.df.options);
 		} else if (!cur_frm) {
