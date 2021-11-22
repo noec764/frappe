32d7e7cc2c92ff89591082d336bbff258b1f7348
Charles-Henri <chdecultot@dokos.io>
2021-11-22 17:52:33 +0100

    fix: Make strings translatable

--- frappe/public/js/frappe/ui/toolbar/awesome_bar.js
+++ frappe/public/js/frappe/ui/toolbar/awesome_bar.js
@@ -315,10 +315,10 @@ frappe.search.AwesomeBar = class AwesomeBar {
 	make_random(txt) {
 		if(txt.toLowerCase().includes('random')) {
 			this.options.push({
-				label: "Generate Random Password",
+				label: __("Generate Random Password"),
 				value: frappe.utils.get_random(16),
 				onclick: function() {
-					frappe.msgprint(frappe.utils.get_random(16), "Result");
+					frappe.msgprint(frappe.utils.get_random(16), __("Result"));
 				}
 			})
 		}
