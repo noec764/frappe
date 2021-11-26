9cd086116d9a21379e6987ecc2da5b73b9353b69
Charles-Henri <chdecultot@dokos.io>
2021-11-26 16:41:23 +0100

    feat: Option to disable Event Reminders in Notification Settings

--- frappe/desk/doctype/notification_settings/test_notification_settings.py
+++ frappe/desk/doctype/notification_settings/test_notification_settings.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2021, Dokos SAS and Contributors
-# License: MIT. See LICENSE
-
+# Copyright (c) 2021, Frappe Technologies and Contributors
+# See license.txt
 
 # import frappe
 import unittest
