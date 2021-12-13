92e9a8483ae4e3a5e603397be88908a9a0eeede6
Charles-Henri <chdecultot@dokos.io>
2021-12-13 08:36:57 +0100

    feat: sanitise frappe.qb

--- frappe/query_builder/utils.py
+++ frappe/query_builder/utils.py
@@ -10,6 +10,7 @@ import frappe
 from .builder import MariaDB, Postgres
 from pypika.terms import PseudoColumn
 
+from frappe.query_builder.terms import NamedParameterWrapper
 
 class db_type_is(Enum):
 	MARIADB = "mariadb"
@@ -55,10 +56,15 @@ def patch_query_execute():
 	"""
 
 	def execute_query(query, *args, **kwargs):
-		query = str(query)
+		query, params = prepare_query(query)
+		return frappe.db.sql(query, params, *args, **kwargs) # nosemgrep
+
+	def prepare_query(query):
+		params = {}
+		query = query.get_sql(param_wrapper = NamedParameterWrapper(params))
 		if frappe.flags.in_safe_exec and not query.lower().strip().startswith("select"):
 			raise frappe.PermissionError('Only SELECT SQL allowed in scripting')
-		return frappe.db.sql(query, *args, **kwargs)
+		return query, params
 
 	query_class = get_attr(str(frappe.qb).split("'")[1])
 	builder_class = get_type_hints(query_class._builder).get('return')
@@ -67,6 +73,7 @@ def patch_query_execute():
 		raise BuilderIdentificationFailed
 
 	builder_class.run = execute_query
+	builder_class.walk = prepare_query
 
 def patch_query_aggregation():
 	"""Patch aggregation functions to frappe.qb
