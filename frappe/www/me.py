f00c7b0e984cda82af72777a1e3ea775673ec913
Charles-Henri <chdecultot@dokos.io>
2022-01-12 08:17:10 +0100

    refactor(UI): cleanup portal pages

--- frappe/www/me.py
+++ frappe/www/me.py
@@ -12,9 +12,5 @@ def get_context(context):
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 
+	context.current_user = frappe.get_doc("User", frappe.session.user)
 	context.show_sidebar=True
-
-	active_tokens = frappe.get_all("OAuth Bearer Token",\
-		filters=[["user", "=", frappe.session.user]],\
-		fields=["client"], distinct=True, order_by="creation")
-	context.third_party_apps = True if active_tokens else False
