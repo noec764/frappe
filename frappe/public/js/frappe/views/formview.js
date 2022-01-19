d0095b5905bfd162b45c1f84a463440a8263cd5e
Charles-Henri <chdecultot@dokos.io>
2022-01-19 08:21:23 +0100

    fix: Recursive routing for Form route

--- frappe/public/js/frappe/views/formview.js
+++ frappe/public/js/frappe/views/formview.js
@@ -97,6 +97,7 @@ frappe.views.FormFactory = class FormFactory extends frappe.views.Factory {
 		if (new_name===name) {
 			this.render(doctype_layout, name);
 		} else {
+			frappe.route_flags.replace_route = true;
 			frappe.set_route("Form", doctype_layout, new_name);
 		}
 	}
