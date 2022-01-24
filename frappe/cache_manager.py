71bd23378dfaeadc138ae172a3ca19028f4bb43f
Charles-Henri <chdecultot@dokos.io>
2022-01-24 08:00:36 +0100

    fix: correctly compute doctype name from table name

--- frappe/cache_manager.py
+++ frappe/cache_manager.py
@@ -153,7 +153,7 @@ def build_table_count_cache():
 		frappe.qb.from_(information_schema.tables).select(table_name, table_rows)
 	).run(as_dict=True)
 
-	counts = {d.get('name').lstrip('tab'): d.get('count', None) for d in data}
+	counts = {d.get('name').replace('tab', '', 1): d.get('count', None) for d in data}
 	_cache.set_value("information_schema:counts", counts)
 
 	return counts
