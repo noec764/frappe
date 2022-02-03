c9472dcf66dec6c657805be447c75ab1b76092b5
Charles-Henri <chdecultot@dokos.io>
2022-02-03 08:10:43 +0100

    fix: Import moment in datetime.js for web portal

--- frappe/public/js/libs.bundle.js
+++ frappe/public/js/libs.bundle.js
@@ -1,7 +1,6 @@
 import "./jquery-bootstrap";
 import Vue from "vue/dist/vue.esm.js";
-import moment from "moment/min/moment-with-locales.js";
-import momentTimezone from "moment-timezone/builds/moment-timezone-with-data.js";
+import "./lib/moment";
 import io from "socket.io-client/dist/socket.io.slim.js";
 import Sortable from "./lib/Sortable.min";
 import "./lib/leaflet/leaflet.js"
@@ -12,8 +11,6 @@ import "./lib/leaflet/easy-button.js"
 // Don't think jquery.hotkeys is being used anywhere. Will remove this after being sure.
 // import "./lib/jquery/jquery.hotkeys.js";
 
-window.moment = moment;
-window.moment = momentTimezone;
 window.Vue = Vue;
 window.Sortable = Sortable;
 window.io = io;
