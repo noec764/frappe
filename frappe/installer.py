14e187c0e3cd941d56dd72a9f087da3cde214845
Charles-Henri <chdecultot@dokos.io>
2022-02-21 09:11:05 +0100

    fix: Removed verbose flag for gzip

--- frappe/installer.py
+++ frappe/installer.py
@@ -507,10 +507,9 @@ def extract_sql_gzip(sql_gz_path):
 	import subprocess
 
 	try:
-		# kdvf - keep, decompress, verbose, force
 		original_file = sql_gz_path
 		decompressed_file = original_file.rstrip(".gz")
-		cmd = 'gzip -dvf < {0} > {1}'.format(original_file, decompressed_file)
+		cmd = 'gzip --decompress --force < {0} > {1}'.format(original_file, decompressed_file)
 		subprocess.check_call(cmd, shell=True)
 	except Exception:
 		raise
