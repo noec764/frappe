b9c6315f34eacd0746278ad3ee61c57a0d9891e5
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2022-03-17 16:53:26 +0000

    chore(Nextcloud): Add custom exceptions types

---
+++ frappe/integrations/doctype/nextcloud_settings/exceptions.py
@@ -0,0 +1,24 @@
+from frappe import _
+
+class NextcloudException(Exception):
+	pass
+
+class NextcloudSyncMissingRoot(NextcloudException):
+	def __init__(self, *args):
+			super().__init__('no root directory', *args)
+
+class NextcloudSyncCannotCreateRoot(NextcloudException):
+	def __init__(self, *args):
+			super().__init__('failed to create root directory', *args)
+
+class NextcloudSyncCannotFetchRoot(NextcloudException):
+	def __init__(self, *args):
+			super().__init__('failed to fetch root directory', *args)
+
+class NextcloudExceptionServerIsDown(NextcloudException):
+	def __init__(self, *args):
+			super().__init__(_('Nextcloud server is down'), *args)
+
+class NextcloudExceptionInvalidCredentials(NextcloudException):
+	def __init__(self, *args):
+			super().__init__(_('Invalid Credentials'), *args)
