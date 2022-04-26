c0407c11da69041357edca36a9da2bbd68e22f5a
Charles-Henri <chdecultot@dokos.io>
2022-04-26 18:00:53 +0200

    bumped to version 2.12.1

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -27,7 +27,7 @@ from .utils.lazy_loader import lazy_import
 # Lazy imports
 faker = lazy_import('faker')
 
-__version__ = '2.12.0'
+__version__ = '2.12.1'
 __title__ = "Dodock Framework"
 
 local = Local()
