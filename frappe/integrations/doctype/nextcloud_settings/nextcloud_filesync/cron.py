7b607e17f5284de356fa1878e23dfc75066df318
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2022-05-09 13:28:53 +0000

    fix(Nextcloud): Rollback on error and log conflicts

--- frappe/integrations/doctype/nextcloud_settings/nextcloud_filesync/cron.py
+++ frappe/integrations/doctype/nextcloud_settings/nextcloud_filesync/cron.py
@@ -1,10 +1,17 @@
+import re
+
 import frappe
 from frappe import _
 
 from frappe.integrations.doctype.nextcloud_settings import get_nextcloud_settings
 from frappe.integrations.doctype.nextcloud_settings.exceptions import NextcloudException
+from frappe.integrations.doctype.nextcloud_settings.nextcloud_filesync.diff_engine.Action import Action
+from frappe.integrations.doctype.nextcloud_settings.nextcloud_filesync.sync import NextcloudFileSync, sync_log
 
-from .sync import NextcloudFileSync, sync_log
+
+ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+def strip_ansi(s):
+	return ansi_escape.sub('', s)
 
 
 def get_sync_module():
@@ -27,21 +34,45 @@ class NextcloudFilesyncCronRunner():
 		raise e
 
 	def _post_sync(self):
-		self.log(self.syncer.conflicts)
-		frappe.db.commit()
+		if len(self.syncer.conflicts):
+			def fmt_conflict(conflict: Action) -> str:
+				s = ''
+
+				if conflict.local:
+					s += (conflict.local._frappe_name or conflict.local.path) + ' '
+				elif conflict.remote:
+					s += (conflict.remote._file_info and conflict.remote._file_info.path or conflict.remote.path) + ' '
+
+				s += '(' + repr(conflict) + ')'
+				return s
+
+			title = _('File conflicts exist between Dokos and Nextcloud, it is not possible to synchronize those files.')
+
+			conflicts = map(fmt_conflict, self.syncer.conflicts)
+
+			details = _('The following files have conflicts:', context='Nextcloud Integration') \
+				+ '\n * ' + '\n * '.join(conflicts)
+			details = strip_ansi(details)
+
+			frappe.db.rollback()
+			self._error_log(title, details)
+			self.syncer.client.logout()
+			return
+
 		self.syncer.client.logout()
+		frappe.db.commit()
 
 	def _error_log(self, title: str, details: str = ""):
 		self.log('/!\\ ' + title)
 		self.log(details)
 		self.log()
-		msg = "{}: {}".format(_('Nextcloud Integration'), title)
+		msg = "{}: {}".format(_('Nextcloud'), title)
 		frappe.log_error(details, msg)
 
-	def _delete_remote_root(self):
-		p = self.syncer.common.root
-		self.log('deleting root:', p, self.syncer.common.cloud_client.delete(p))
-		self.syncer._unjoin_all_files()
+	# def _delete_remote_root(self):
+	# 	p = self.syncer.common.root
+	# 	self.log('deleting root:', p, self.syncer.common.cloud_client.delete(p))
+	# 	self.syncer._unjoin_all_files()
 
 	def run(self):
 		self.log('--- CRON RUN ---')
@@ -52,7 +83,6 @@ class NextcloudFilesyncCronRunner():
 
 		try:
 			self._run()
-			# raise Exception('ok')
 			self._post_sync()
 		except Exception as e:
 			self._on_sync_error(e)
@@ -65,26 +95,25 @@ class NextcloudFilesyncCronRunner():
 
 		if s.status == 'error':
 			if s.message == 'home-different-ids':
-				self._error_log('Identifiants différents', repr(s))
+				self._error_log(repr(s))
 			else:
 				raise NextcloudException(s.message)
 
 		if s.sync_type == 'none':
 			return
 		elif s.sync_type == 'normal':
-			self.syncer.complete_sync(conflict_strategy='stop')
+			self.syncer.complete_sync(conflict_strategy='stop', commit=False)
 		elif s.sync_type == 'all':
-			self.syncer.complete_sync(conflict_strategy='stop', down_sync_all=True)
+			self.syncer.complete_sync(conflict_strategy='stop', down_sync_all=True, commit=False)
 		elif s.sync_type == 'initial':
-			self.syncer.complete_sync(conflict_strategy='stop', down_sync_all=True, force_upload=True)
+			self.syncer.complete_sync(conflict_strategy='stop', down_sync_all=True, force_upload=True, commit=False)
 		elif s.sync_type == 'overwrite':
 			self.syncer.preserve_remote_root_bak()
-			self.syncer.complete_sync(conflict_strategy='stop', down_sync_all=True, force_upload=True)
+			self.syncer.complete_sync(conflict_strategy='stop', down_sync_all=True, force_upload=True, commit=False)
 		else:
 			raise ValueError('unknown sync type in' + s)
 
 
-@frappe.whitelist()
 def run_cron():
 	t = frappe.utils.now_datetime()
 	NextcloudFilesyncCronRunner(get_sync_module()).run()
