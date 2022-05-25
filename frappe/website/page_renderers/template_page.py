8570fde0b8766dcebd3562b835c51688c4a9a9b5
Charles-Henri <chdecultot@dokos.io>
2022-05-25 10:06:55 +0200

    fix: Portal User interface

--- frappe/website/page_renderers/template_page.py
+++ frappe/website/page_renderers/template_page.py
@@ -103,8 +103,7 @@ class TemplatePage(BaseTemplatePage):
 		super(TemplatePage, self).post_process_context()
 
 	def add_sidebar_and_breadcrumbs(self):
-		if self.basepath:
-			self.context.sidebar_items = get_sidebar_items(self.context.website_sidebar, self.basepath)
+		self.context.sidebar_items = get_sidebar_items(self.context.website_sidebar, self.basepath)
 
 		if self.context.add_breadcrumbs and not self.context.parents:
 			parent_path = os.path.dirname(self.path)
