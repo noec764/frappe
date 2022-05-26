6e0eb8791cd20bd7379cadf221c265be7bfc9e8d
Charles-Henri <chdecultot@dokos.io>
2022-05-26 19:10:05 +0200

    fix: Add condition for website me page

--- frappe/www/me.py
+++ frappe/www/me.py
@@ -13,5 +13,14 @@ def get_context(context):
 	if frappe.session.user == "Guest":
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 
+	active_tokens = frappe.get_all(
+		"OAuth Bearer Token",
+		filters=[["user", "=", frappe.session.user]],
+		fields=["client"],
+		distinct=True,
+		order_by="creation",
+	)
+	context.third_party_apps = True if active_tokens else False
+
 	context.current_user = frappe.get_doc("User", frappe.session.user)
 	context.show_sidebar = True
