e2cc32ab08985080007b325758b82f209f695953
Charles-Henri <chdecultot@dokos.io>
2022-06-02 22:10:53 +0200

    fix: Typo in doctype

--- frappe/translate.py
+++ frappe/translate.py
@@ -1143,7 +1143,7 @@ def get_all_languages(with_language_name=False):
 		return frappe.get_all("Language", pluck="name")
 
 	def get_all_language_with_name():
-		return frappe.db.get_all("language", ["language_code", "language_name"])
+		return frappe.db.get_all("Language", ["language_code", "language_name"])
 
 	if not frappe.db:
 		frappe.connect()
