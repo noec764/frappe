14f16a51d6e061ac886f3c00cae6169527cc4775
Charles-Henri <chdecultot@dokos.io>
2022-06-15 18:14:16 +0200

    fix: Don't add data import headers to exported data

--- frappe/core/doctype/data_export/data_export.js
+++ frappe/core/doctype/data_export/data_export.js
@@ -57,7 +57,7 @@ const export_data = frm => {
 			select_columns: JSON.stringify(columns),
 			filters: frm.filter_list.get_filters().map(filter => filter.slice(1, 4)),
 			file_type: frm.doc.file_type,
-			template: true,
+			template: false,
 			with_data: 1
 		};
 	};
