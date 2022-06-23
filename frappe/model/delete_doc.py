385b0864c96e68262adedeb12a2ed26497a4df47
Charles-Henri <chdecultot@dokos.io>
2022-06-23 17:39:44 +0200

    fix: Virtual DocTypes currently breaking Parent-DocType Update and Deletion

--- frappe/model/delete_doc.py
+++ frappe/model/delete_doc.py
@@ -210,7 +210,9 @@ def delete_from_table(doctype: str, name: str, ignore_doctypes: List[str], doc):
 		frappe.db.delete(doctype, {"name": name})
 
 	if doc:
-		child_doctypes = [d.options for d in doc.meta.get_table_fields()]
+		child_doctypes = [
+			d.options for d in doc.meta.get_table_fields() if frappe.get_meta(d.options).is_virtual == 0
+		]
 
 	else:
 		child_doctypes = frappe.get_all(
