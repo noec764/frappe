7605e11fa9baa177e247b19596722a888abf0cb6
Charles-Henri <chdecultot@dokos.io>
2022-06-28 09:30:17 +0200

    fix: extra column in excel after exporting report with group by

--- frappe/desk/reportview.py
+++ frappe/desk/reportview.py
@@ -156,8 +156,6 @@ def setup_group_by(data):
 					**data
 				)
 			)
-			if data.aggregate_on_field:
-				data.fields.append(f"`tab{data.aggregate_on_doctype}`.`{data.aggregate_on_field}`")
 		else:
 			raise_invalid_field(data.aggregate_on_field)
 
@@ -443,11 +441,20 @@ def append_totals_row(data):
 def get_labels(fields, doctype):
 	"""get column labels based on column names"""
 	labels = []
+	doctype = doctype.lower()
 	for key in fields:
-		key = key.split(" as ")[0]
+		aggregate_function = ""
+
+		key = key.casefold().split(" as ", maxsplit=1)[0]
 
 		if key.startswith(("count(", "sum(", "avg(")):
-			continue
+			# Get aggregate function and _aggregate_column
+			# key = 'sum(`tabDocType`.`fieldname`)'
+			if not key.rstrip().endswith(")"):
+				continue
+			_agg_fn, _key = key.split("(", maxsplit=1)
+			aggregate_function = _agg_fn.lower()  # aggregate_function = 'sum'
+			key = _key[:-1]  # key = `tabDocType`.`fieldname`
 
 		if "." in key:
 			parenttype, fieldname = key.split(".")[0][4:-1], key.split(".")[1].strip("`")
@@ -464,6 +471,11 @@ def get_labels(fields, doctype):
 				# If the column is from a child table, append the child doctype.
 				# For example, "Item Code (Sales Invoice Item)".
 				label += f" ({ _(parenttype) })"
+
+		if aggregate_function:
+			label = _("{0} of {1}").format(aggregate_function.capitalize(), label)
+
+		labels.append(label)
 
 	return labels
 
