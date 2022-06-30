0afce8668f1671336e21daa6c16f7d9cead49c4e
Charles-Henri <chdecultot@dokos.io>
2022-06-30 08:43:31 +0200

    Revert "feat: Adding support to Query engine"
    This reverts commit 7b16a620e5c530e2f7a959ed0c3d329c5576bc7c.

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -12,7 +12,7 @@ from contextlib import contextmanager
 from time import time
 from typing import Dict, List, Optional, Tuple, Union
 
-from pypika.terms import Criterion, NullValue
+from pypika.terms import Criterion, NullValue, PseudoColumn
 
 import frappe
 import frappe.defaults
@@ -90,6 +90,15 @@ class Database(object):
 
 		self.password = password or frappe.conf.db_password
 		self.value_cache = {}
+
+	@property
+	def query(self):
+		if not hasattr(self, "_query"):
+			from .query import Query
+
+			self._query = Query()
+			del Query
+		return self._query
 
 	def setup_type_map(self):
 		pass
@@ -616,7 +625,7 @@ class Database(object):
 				return [map(values.get, fields)]
 
 		else:
-			r = frappe.qb.engine.get_query(
+			r = self.query.get_sql(
 				"Singles",
 				filters={"field": ("in", tuple(fields)), "doctype": doctype},
 				fields=["field", "value"],
@@ -649,7 +658,7 @@ class Database(object):
 		        # Get coulmn and value of the single doctype Accounts Settings
 		        account_settings = frappe.db.get_singles_dict("Accounts Settings")
 		"""
-		queried_result = frappe.qb.engine.get_query(
+		queried_result = self.query.get_sql(
 			"Singles",
 			filters={"doctype": doctype},
 			fields=["field", "value"],
@@ -719,7 +728,7 @@ class Database(object):
 		if cache and fieldname in self.value_cache[doctype]:
 			return self.value_cache[doctype][fieldname]
 
-		val = frappe.qb.engine.get_query(
+		val = self.query.get_sql(
 			table="Singles",
 			filters={"doctype": doctype, "field": fieldname},
 			fields="value",
@@ -761,7 +770,14 @@ class Database(object):
 	):
 		field_objects = []
 
-		query = frappe.qb.engine.get_query(
+		if not isinstance(fields, Criterion):
+			for field in fields:
+				if "(" in str(field) or " as " in str(field):
+					field_objects.append(PseudoColumn(field))
+				else:
+					field_objects.append(field)
+
+		query = self.query.get_sql(
 			table=doctype,
 			filters=filters,
 			orderby=order_by,
@@ -870,7 +886,7 @@ class Database(object):
 					frappe.clear_document_cache(dt, docname)
 
 			else:
-				query = frappe.qb.engine.build_conditions(table=dt, filters=dn, update=True)
+				query = self.query.build_conditions(table=dt, filters=dn, update=True)
 				# TODO: Fix this; doesn't work rn - gavin@frappe.io
 				# frappe.cache().hdel_keys(dt, "document_cache")
 				# Workaround: clear all document caches
@@ -1064,9 +1080,7 @@ class Database(object):
 			cache_count = frappe.cache().get_value("doctype:count:{}".format(dt))
 			if cache_count is not None:
 				return cache_count
-		query = frappe.qb.engine.get_query(
-			table=dt, filters=filters, fields=Count("*"), distinct=distinct
-		)
+		query = self.query.get_sql(table=dt, filters=filters, fields=Count("*"), distinct=distinct)
 		count = self.sql(query, debug=debug)[0][0]
 		if not filters and cache:
 			frappe.cache().set_value("doctype:count:{}".format(dt), count, expires_in_sec=86400)
@@ -1206,7 +1220,7 @@ class Database(object):
 		Doctype name can be passed directly, it will be pre-pended with `tab`.
 		"""
 		filters = filters or kwargs.get("conditions")
-		query = frappe.qb.engine.build_conditions(table=doctype, filters=filters).delete()
+		query = self.query.build_conditions(table=doctype, filters=filters).delete()
 		if "debug" not in kwargs:
 			kwargs["debug"] = debug
 		return query.run(**kwargs)
--- frappe/query_builder/__init__.py
+++ frappe/query_builder/__init__.py
@@ -1,13 +1,12 @@
 import pypika.terms
-from pypika import *  # noqa
+from pypika import *
 from pypika import Field
 from pypika.utils import ignore_copy
 
 from frappe.query_builder.terms import ParameterizedFunction, ParameterizedValueWrapper
-from frappe.query_builder.utils import (  # noqa
+from frappe.query_builder.utils import (
 	Column,
 	DocType,
-	get_qb_engine,
 	get_query_builder,
 	patch_query_aggregation,
 	patch_query_execute,
