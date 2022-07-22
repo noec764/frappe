5be733f427b24ac822644265fe272934fc5d1349
Charles-Henri <chdecultot@dokos.io>
2022-07-22 17:49:07 +0200

    fix: Currency symbol position

--- frappe/tests/test_formatter.py
+++ frappe/tests/test_formatter.py
@@ -12,7 +12,7 @@ class TestFormatter(unittest.TestCase):
 		frappe.db.set_default("currency", "INR")
 
 		# if currency field is not passed then default currency should be used.
-		self.assertEqual(format(100000, df, doc, format="#,###.##"), "100,000.00 ₹")
+		self.assertEqual(format(100000, df, doc, format="#,###.##"), "₹ 100,000.00")
 
 		doc.currency = "USD"
 		self.assertEqual(format(100000, df, doc, format="#,###.##"), "100,000.00 $")
