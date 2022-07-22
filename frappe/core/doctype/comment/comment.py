b32915501983b12fd9557a4ce9bf6682c0bcf1e7
Charles-Henri <chdecultot@dokos.io>
2022-07-22 15:39:56 +0200

    refactor!: Virtual DocType

--- frappe/core/doctype/comment/comment.py
+++ frappe/core/doctype/comment/comment.py
@@ -13,6 +13,7 @@ from frappe.desk.doctype.notification_log.notification_log import (
 )
 from frappe.exceptions import ImplicitCommitError
 from frappe.model.document import Document
+from frappe.model.utils import is_virtual_doctype
 from frappe.utils import get_fullname
 from frappe.website.utils import clear_cache
 
@@ -158,7 +159,10 @@ def get_comments_from_parent(doc):
 	`_comments`
 	"""
 	try:
-		_comments = frappe.db.get_value(doc.reference_doctype, doc.reference_name, "_comments") or "[]"
+		if is_virtual_doctype(doc.reference_doctype):
+			_comments = "[]"
+		else:
+			_comments = frappe.db.get_value(doc.reference_doctype, doc.reference_name, "_comments") or "[]"
 
 	except Exception as e:
 		if frappe.db.is_missing_table_or_column(e):
@@ -181,7 +185,7 @@ def update_comments_in_parent(reference_doctype, reference_name, _comments):
 		not reference_doctype
 		or not reference_name
 		or frappe.db.get_value("DocType", reference_doctype, "issingle")
-		or frappe.db.get_value("DocType", reference_doctype, "is_virtual")
+		or is_virtual_doctype(reference_doctype)
 	):
 		return
 
