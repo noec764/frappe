d2170480e9e36dec084bb2a42312c96149048ee1
Charles-Henri <chdecultot@dokos.io>
2022-08-03 15:51:56 +0200

    chore: Minified DocType JSON notation from old verbose notation

---
+++ frappe/desk/doctype/list_filter/list_filter.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Dokos SAS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('List Filter', {
+	// refresh: function(frm) {
+
+	// }
+});
