2b2643f2278e5f58ffb90cd787dc9660596d06a0
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2022-08-17 16:50:30 +0000

    fix: Fix links of the raise_link_exists_exception modal

--- frappe/model/delete_doc.py
+++ frappe/model/delete_doc.py
@@ -11,6 +11,7 @@ from frappe.desk.doctype.tag.tag import delete_tags_for_document
 from frappe.model.dynamic_links import get_dynamic_link_map
 from frappe.model.naming import revert_series_if_last
 from frappe.model.utils import is_virtual_doctype
+from frappe.utils.data import get_link_to_form
 from frappe.utils.file_manager import remove_all
 from frappe.utils.global_search import delete_for_document
 from frappe.utils.password import delete_all_passwords_for
@@ -367,10 +368,8 @@ def check_if_doc_is_dynamically_linked(doc, method="Delete"):
 
 
 def raise_link_exists_exception(doc, reference_doctype, reference_docname, row=""):
-	doc_link = f'<a href="/app/{doc.doctype}/{doc.name}">{_(doc.name)}</a>'
-	reference_link = '<a href="#Form/{}/{}">{}</a>'.format(
-		reference_doctype, reference_docname, _(reference_docname)
-	)
+	doc_link = get_link_to_form(doc.doctype, doc.name, _(doc.name))
+	reference_link = get_link_to_form(reference_doctype, reference_docname, _(reference_docname))
 
 	# hack to display Single doctype only once in message
 	if reference_doctype == reference_docname:
