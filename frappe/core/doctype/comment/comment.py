ce60a072823d897ccb2577bf0fdb3251bdfaf5dc
Charles-Henri Decultot <chdecultot@dokos.io>
2022-09-02 15:07:50 +0200

    refactor: dont fetch whole doc for 1 value

--- frappe/core/doctype/comment/comment.py
+++ frappe/core/doctype/comment/comment.py
@@ -165,12 +165,13 @@ def update_comments_in_parent(reference_doctype, reference_name, _comments):
 
 		else:
 			raise ImplicitCommitError
-
 	else:
-		if not (frappe.flags.in_patch or frappe.flags.in_test):
-			reference_doc = frappe.get_doc(reference_doctype, reference_name)
-			if getattr(reference_doc, "route", None):
-				clear_cache(reference_doc.route)
+		if frappe.flags.in_patch:
+			return
+
+		# Clear route cache
+		if route := frappe.get_cached_value(reference_doctype, reference_name, "route"):
+			clear_cache(route)
 
 
 def update_comments_in_parent_after_request():
