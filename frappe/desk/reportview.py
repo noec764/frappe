e774233d53f0e8ee87b404b7d717a4c6d4aad6e8
Charles-Henri Decultot <chdecultot@dokos.io>
2022-11-10 09:55:56 +0100

    fix: reportview permlevel bug

--- frappe/desk/reportview.py
+++ frappe/desk/reportview.py
@@ -4,6 +4,7 @@
 
 """build query for doclistview and return results"""
 
+import copy
 import json
 from io import StringIO
 
@@ -90,9 +91,10 @@ def validate_args(data):
 
 
 def validate_fields(data):
+	data_fields = copy.copy(data.fields)
 	wildcard = update_wildcard_field_param(data)
 
-	for field in data.fields or []:
+	for field in data_fields or []:
 		fieldname = extract_fieldname(field)
 		if is_standard(fieldname):
 			continue
