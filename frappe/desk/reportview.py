edb4ef0f3cacd22a8436b9d590fb6b265bc49cf2
Charles-Henri Decultot <chdecultot@dokos.io>
2022-11-10 09:57:00 +0100

    refactor: validation of permlevel

--- frappe/desk/reportview.py
+++ frappe/desk/reportview.py
@@ -4,7 +4,6 @@
 
 """build query for doclistview and return results"""
 
-import copy
 import json
 from io import StringIO
 
@@ -91,10 +90,9 @@ def validate_args(data):
 
 
 def validate_fields(data):
-	data_fields = copy.copy(data.fields)
 	wildcard = update_wildcard_field_param(data)
 
-	for field in data_fields or []:
+	for field in list(data.fields or []):
 		fieldname = extract_fieldname(field)
 		if is_standard(fieldname):
 			continue
