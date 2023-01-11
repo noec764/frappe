134db1071ff3b86c8cb20f3e170e1fe75d8d14a4
Charles-Henri Decultot <chdecultot@dokos.io>
2023-01-11 12:22:17 +0100

    fix: Imports in web forms

--- frappe/public/js/web_form.bundle.js
+++ frappe/public/js/web_form.bundle.js
@@ -1,3 +1,16 @@
-import "./lib/moment.js";
+import "./controls.bundle.js";
+import "./frappe/model/create_new.js";
+import "./dialog.bundle.js";
+import "./lib/moment";
 import "./frappe/utils/datetime.js";
 import "./frappe/web_form/webform_script.js";
+import "./bootstrap-4-web.bundle.js";
+
+import Vue from "vue/dist/vue.esm.js";
+import Sortable from "sortablejs";
+
+window.Vue = Vue;
+Vue.prototype.__ = window.__;
+Vue.prototype.frappe = window.frappe;
+
+window.Sortable = Sortable;
