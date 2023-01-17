02cd26eb03534a4ae250b693f28b4c0552d0cf1d
Charles-Henri Decultot <chdecultot@dokos.io>
2023-01-17 08:08:43 +0100

    chore: Add tours for energy points related doctypes

---
+++ frappe/social/doctype/energy_point_rule/energy_point_rule_tour.js
@@ -0,0 +1,36 @@
+frappe.tour["Energy Point Rule"] = {
+	fr: [
+		{
+			fieldname: "enabled",
+			description: "Active/Désactive la règle d'attribution de points d'énergie",
+		},
+		{
+			fieldname: "reference_doctype",
+			description: "Type de document de référence déclenchant la règle.",
+		},
+		{
+			fieldname: "for_doc_event",
+			description:
+				"Evénement permettant le déclenchement de la règle.<br><i>Ex. <strong>Nouveau</strong>: La création d'un nouveau document déclenche l'attribution de points.</i>",
+		},
+		{
+			fieldname: "points",
+			description: "Nombre de points attribués",
+		},
+		{
+			fieldname: "user_field",
+			description:
+				"Champ contenant la référence de l'utilisateur à qui attribuer les points",
+		},
+		{
+			fieldname: "multiplier_field",
+			description:
+				"Champ contenant un nombre entier ou réel permettant de multiplier le nombre de points à attribuer",
+		},
+		{
+			fieldname: "apply_only_once",
+			description:
+				"Option permettant de n'appliquer la règle qu'une seule fois pour chaque document",
+		},
+	],
+};
