3d3ff6d537d52c352584eb4c70b0639871cc533d
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-02-13 15:44:48 +0100

    fix!(event portal): Convert all routes to absolute routes

--- frappe/public/js/frappe/events_portal/index.js
+++ frappe/public/js/frappe/events_portal/index.js
@@ -109,7 +109,8 @@ frappe.events.EventsPortalView = class EventsPortalView {
 
 		event.event.extendedProps.route &&
 			dialog.set_primary_action(__("See details"), () => {
-				window.location.href = event.event.extendedProps.route;
+				const absolutePath = event.event.extendedProps.route.replace(/^\/?/, "/")
+				window.location.href = absolutePath;
 			});
 		dialog.fields_dict.event_description.$wrapper.html(event_details(event));
 		dialog.show();
