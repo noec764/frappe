f122462f29cc0e309837c13c1534a35b27b4ccb0
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-02-17 16:10:46 +0100

    fix: Rename test_base_document.py

--- frappe/tests/test_base_document.py
+++ frappe/tests/test_base_document.py
@@ -1,10 +1,9 @@
-from frappe.tests.utils import FrappeTestCase
-
 from frappe.model.base_document import BaseDocument
+from frappe.tests.utils import FrappeTestCase
 
 
 class TestBaseDocument(FrappeTestCase):
-	def test_docstatus(self):
+	def test_check_docstatus_methods(self):
 		doc = BaseDocument({"docstatus": 0, "doctype": "ToDo"})
 		self.assertTrue(doc.docstatus.is_draft())
 		self.assertEqual(doc.docstatus, 0)
