f913e8bc2dbdff6c5d6a3824095b774333f8c65c
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-03-28 16:08:43 +0200

    fix(awesomebar): Search tags immediately on "#" input

--- frappe/public/js/frappe/ui/toolbar/awesome_bar.js
+++ frappe/public/js/frappe/ui/toolbar/awesome_bar.js
@@ -66,6 +66,8 @@ frappe.search.AwesomeBar = class AwesomeBar {
 					me.add_defaults(txt);
 					me.options = me.options.concat(me.build_options(txt));
 					me.options = me.options.concat(me.global_results);
+				} else if (txt === "#") {
+					me.options = me.options.concat(frappe.tags.utils.get_tags(txt));
 				} else {
 					me.options = me.options.concat(
 						me.deduplicate(frappe.search.utils.get_recent_pages(txt || ""))
