de5297ee33f10fb8b8ad7d787558a2807ad58722
Charles-Henri Decultot <chdecultot@dokos.io>
2023-04-11 11:59:24 +0200

    feat: New chart 'Connections by user type'

---
+++ frappe/core/report/user_connections/user_connections.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2023, Dokos SAS and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["User Connections"] = {
+	filters: [],
+};
