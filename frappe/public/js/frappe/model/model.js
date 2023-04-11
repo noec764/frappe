27224a4724c7398c85e8dfc0e140354ce5014452
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-04-11 15:54:45 +0200

    refactor(customize_form): Use get_views_of_doctype in set_default_views_for_doctype

--- frappe/public/js/frappe/model/model.js
+++ frappe/public/js/frappe/model/model.js
@@ -829,37 +829,7 @@ $.extend(frappe.model, {
 	},
 
 	set_default_views_for_doctype(doctype, frm) {
-		frappe.model.with_doctype(doctype, () => {
-			let meta = frappe.get_meta(doctype);
-			let default_views = ["List", "Report", "Dashboard", "Kanban"];
-
-			if (meta.is_calendar_and_gantt && frappe.views.calendar[doctype]) {
-				let views = ["Calendar", "Gantt"];
-				default_views.push(...views);
-			}
-
-			if (meta.is_tree) {
-				default_views.push("Tree");
-			}
-
-			if (frm.doc.image_field) {
-				default_views.push("Image");
-			}
-
-			if (doctype === "Communication" && frappe.boot.email_accounts.length) {
-				default_views.push("Inbox");
-			}
-
-			if (
-				(frm.doc.fields.find((i) => i.fieldname === "latitude") &&
-					frm.doc.fields.find((i) => i.fieldname === "longitude")) ||
-				frm.doc.fields.find(
-					(i) => i.fieldname === "location" && i.fieldtype == "Geolocation"
-				)
-			) {
-				default_views.push("Map");
-			}
-
+		this.get_views_of_doctype(doctype).then((default_views) => {
 			frm.set_df_property("default_view", "options", default_views);
 		});
 	},
