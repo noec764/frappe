83bb91d83f11e44b3b0b482315224ab982ad0a25
Charles-Henri Decultot <chdecultot@dokos.io>
2023-04-14 15:09:01 +0200

    fix: Translations

--- frappe/translate.py
+++ frappe/translate.py
@@ -449,7 +449,7 @@ def get_messages_for_app(app, deduplicate=True, context=False):
 		workspace = DocType("Workspace")
 		for name in (
 			frappe.qb.from_(workspace)
-			.where(Field("module").isin(modules))
+			.where(Field("module").isin(modules) & (workspace.is_standard == 1))
 			.select(workspace.name)
 			.run(pluck=True)
 		):
@@ -463,6 +463,15 @@ def get_messages_for_app(app, deduplicate=True, context=False):
 			.run(pluck=True)
 		):
 			messages.append(("Dashboard Chart: " + name, name))
+
+		number_card = DocType("Number Card")
+		for name in (
+			frappe.qb.from_(number_card)
+			.where(Field("module").isin(modules) & (number_card.is_standard == 1))
+			.select(number_card.name)
+			.run(pluck=True)
+		):
+			messages.append(("Number Card: " + name, name))
 
 	if app == "frappe":
 		# Web templates
