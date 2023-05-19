574981ed72162e0cb169adb603b8d32444b9db1a
Charles-Henri Decultot <chdecultot@dokos.io>
2023-05-19 14:42:37 +0200

    fix: using 'Equals' and 'Is' on multiselect throws error if 'get_filters_cond' is used for formating filters

--- frappe/desk/reportview.py
+++ frappe/desk/reportview.py
@@ -719,6 +719,7 @@ def get_filters_cond(
 				if isinstance(f[1], str) and f[1][0] == "!":
 					flt.append([doctype, f[0], "!=", f[1][1:]])
 				elif isinstance(f[1], (list, tuple)) and f[1][0].lower() in (
+					"=",
 					">",
 					"<",
 					">=",
@@ -729,6 +730,7 @@ def get_filters_cond(
 					"in",
 					"not in",
 					"between",
+					"is",
 				):
 
 					flt.append([doctype, f[0], f[1][0], f[1][1]])
