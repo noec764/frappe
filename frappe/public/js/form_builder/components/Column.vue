4f45de33c03cab027a919373b4483c04f71c5f11
Charles-Henri Decultot <chdecultot@dokos.io>
2023-06-09 09:05:48 +0200

    feat: support reordering standard fields in Form Builder

--- frappe/public/js/form_builder/components/Column.vue
+++ frappe/public/js/form_builder/components/Column.vue
@@ -147,8 +147,6 @@ function move_columns_to_section() {
 			class="column-container"
 			:style="{ backgroundColor: column.fields.length ? '' : 'var(--field-placeholder-color)' }"
 			v-model="column.fields"
-			group="fields"
-			filter="[data-is-custom='0']"
 			:prevent-on-filter="false"
 			:animation="200"
 			:easing="store.get_animation"
@@ -159,7 +157,7 @@ function move_columns_to_section() {
 				<Field
 					:column="column"
 					:field="element"
-					:data-is-custom="element.df.is_custom_field"
+					:data-is-user-generated="store.is_user_generated_field(element)"
 				/>
 			</template>
 		</draggable>
