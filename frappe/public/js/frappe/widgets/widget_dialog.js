6ecbb09b470d3f24b3953827c0e7759ec579e85a
Ankush Menat <ankush@frappe.io>
2023-08-10 17:37:30 +0530

    chore: typo

--- frappe/public/js/frappe/widgets/widget_dialog.js
+++ frappe/public/js/frappe/widgets/widget_dialog.js
@@ -588,7 +588,7 @@ class NumberCardDialog extends WidgetDialog {
 				{
 					fieldtype: "Link",
 					fieldname: "number_card_name",
-					label: __("Number Cards"),
+					label: __("Number Card"),
 					options: "Number Card",
 					reqd: 1,
 					get_query: () => {
