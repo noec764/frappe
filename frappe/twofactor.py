81e034de248179b05e771cda43d16805e5026c25
Charles-Henri Decultot <chdecultot@dokos.io>
2023-08-10 17:31:52 +0000

    fix: missing comma

--- frappe/twofactor.py
+++ frappe/twofactor.py
@@ -275,7 +275,7 @@ def get_email_body_for_2fa(kwargs_dict):
 		+ """
 		<br><br>
 		<b style="font-size: 18px;">{{ otp }}</b>
-	"""
+	""")
 	return frappe.render_template(body_template, kwargs_dict)
 
 
