07aa8d57d915c87736ff2a9d24445973443a5212
Charles-Henri Decultot <chdecultot@dokos.io>
2023-08-10 11:17:36 +0200

    refactor: Vue in web form bundle

--- frappe/public/js/web_form.bundle.js
+++ frappe/public/js/web_form.bundle.js
@@ -6,7 +6,11 @@ import "./frappe/utils/datetime.js";
 import "./frappe/web_form/webform_script.js";
 import "./bootstrap-4-web.bundle.js";
 
-import "./frappe_vue.js";
+window.SetVueGlobals = (app) => {
+	app.config.globalProperties.__ = window.__;
+	app.config.globalProperties.frappe = window.frappe;
+};
+
 import Sortable from "sortablejs";
 
 window.Sortable = Sortable;
