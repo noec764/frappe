28793245a76ccb95a4b937c2394cf4c531fa5406
Ankush Menat <ankush@frappe.io>
2023-08-12 10:55:35 +0530

    fix: fair-er scheduling for multitenant deploys (#22022)

--- frappe/utils/scheduler.py
+++ frappe/utils/scheduler.py
@@ -12,6 +12,7 @@ Events:
 
 # imports - standard imports
 import os
+import random
 import time
 from typing import NoReturn
 
@@ -53,6 +54,9 @@ def enqueue_events_for_all_sites() -> None:
 
 	with frappe.init_site():
 		sites = get_sites()
+
+	# Sites are sorted in alphabetical order, shuffle to randomize priorities
+	random.shuffle(sites)
 
 	for site in sites:
 		try:
