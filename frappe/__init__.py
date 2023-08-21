041faa1ca54f7676753faeef4926193100c9d83b
barredterra <14891507+barredterra@users.noreply.github.com>
2023-08-21 17:13:35 +0200

    feat: return absolute pymodule path (#22142)

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -1427,10 +1427,12 @@ def get_pymodule_path(modulename, *joins):
 
 	:param modulename: Python module name.
 	:param *joins: Join additional path elements using `os.path.join`."""
+	from os.path import abspath, dirname, join
+
 	if joins and "public" not in joins and "www" not in joins[0]:
 		joins = [scrub(part) for part in joins]
 
-	return os.path.join(os.path.dirname(get_module(scrub(modulename)).__file__ or ""), *joins)
+	return abspath(join(dirname(get_module(scrub(modulename)).__file__ or ""), *joins))
 
 
 def get_module_list(app_name):
