95d6f2cee3fea5bb4f1cabf790ddc6a45baabc80
Ankush Menat <ankush@frappe.io>
2023-08-28 12:31:25 +0530

    fix: misc dx improvemnts (#22188)
    * fix(dx): simplify adding callbacks
    
    frappe.db.after_commit(func) == frappe.db.after_commit.add(func)
    
    * fix: trace id missing
    
    DB gets initted before request

--- frappe/utils/__init__.py
+++ frappe/utils/__init__.py
@@ -1149,6 +1149,9 @@ class CallbackManager:
 		"""Add a function to queue, functions are executed in order of addition."""
 		self._functions.append(func)
 
+	def __call__(self, func: Callable) -> None:
+		self.add(func)
+
 	def run(self):
 		"""Run all functions in queue"""
 		while self._functions:
