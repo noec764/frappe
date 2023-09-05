621d4a8b3a4696510988a8b15840e94159af77e0
Ankush Menat <ankush@frappe.io>
2023-09-05 13:05:25 +0530

    test: basic tests v2 API

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -836,7 +836,7 @@ def get_timezones():
 
 
 @frappe.whitelist()
-def get_all_roles(arg=None):
+def get_all_roles():
 	"""return all roles"""
 	active_domains = frappe.get_active_domains()
 
@@ -850,7 +850,7 @@ def get_all_roles(arg=None):
 		order_by="name",
 	)
 
-	return [role.name for role in roles]
+	return sorted([role.get("name") for role in roles])
 
 
 @frappe.whitelist()
