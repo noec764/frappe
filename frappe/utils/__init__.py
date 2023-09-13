489bb5de9b443b3bcbb4ee79ecc3874cc9efb3e4
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-09-13 15:14:02 +0200

    Revert "refactor!: merge get_site_url into get_url (#22308)"
    This reverts commit 31af3ef122f9a246937a25948c978ab8dbeea53c.

--- frappe/utils/__init__.py
+++ frappe/utils/__init__.py
@@ -540,9 +540,11 @@ def get_request_site_address(full_address=False):
 	return get_url(full_address=full_address)
 
 
-@deprecated
 def get_site_url(site):
-	return frappe.utils.get_url()
+	conf = frappe.get_conf(site)
+	if conf.host_name:
+		return conf.host_name
+	return f"http://{site}:{conf.webserver_port}"
 
 
 def encode_dict(d, encoding="utf-8"):
