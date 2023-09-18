bdd04d9711891c1d82767efc314b6fbd2e2a351a
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-09-18 18:18:40 +0200

    fix(tab): Remove fade transition

--- frappe/public/js/frappe/form/tab.js
+++ frappe/public/js/frappe/form/tab.js
@@ -27,7 +27,7 @@ export default class Tab {
 			</li>
 		`).appendTo(this.tab_link_container);
 
-		this.wrapper = $(`<div class="tab-pane fade show ${this.df.active ? "active" : ""}"
+		this.wrapper = $(`<div class="tab-pane show ${this.df.active ? "active" : ""}"
 			id="${id}" role="tabpanel" aria-labelledby="${id}-tab">`).appendTo(this.tabs_content);
 	}
 
