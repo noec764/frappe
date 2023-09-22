3744baaba03c2793d11699ecc5d678f432cb7e7b
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-09-22 15:34:47 +0200

    feat: Add FRAPPE_TRACE_ID to queries

--- frappe/database/database.py
+++ frappe/database/database.py
@@ -96,6 +96,10 @@ class Database:
 		self.before_rollback = CallbackManager()
 		self.after_rollback = CallbackManager()
 
+		self._trace_comment = ""
+		if trace_id := get_trace_id():
+			self._trace_comment = f" /* FRAPPE_TRACE_ID: {trace_id} */"
+
 		# self.db_type: str
 		# self.last_query (lazy) attribute of last sql query executed
 
@@ -208,6 +212,7 @@ class Database:
 			values = (values,)
 
 		query, values = self._transform_query(query, values)
+		query += self._trace_comment
 
 		if trace_id := get_trace_id():
 			query += f" /* FRAPPE_TRACE_ID: {trace_id} */"
