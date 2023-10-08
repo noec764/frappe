7b9fa9ae6553d800e13dd4044839d56887ecebb6
ruthra kumar <ruthra@erpnext.com>
2023-10-08 17:22:29 +0530

    fix: keyerror on reports with subtotal

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -208,11 +208,7 @@ def run(
 	if sbool(are_default_filters) and report.custom_filters:
 		filters = report.custom_filters
 
-	if (
-		report.prepared_report
-		and not sbool(ignore_prepared_report)
-		and not custom_columns
-	):
+	if report.prepared_report and not sbool(ignore_prepared_report) and not custom_columns:
 		if filters:
 			if isinstance(filters, str):
 				filters = json.loads(filters)
@@ -579,7 +575,9 @@ def get_filtered_data(ref_doctype, columns, data, user):
 	if match_filters_per_doctype:
 		for row in data:
 			# Why linked_doctypes.get(ref_doctype)? because if column is empty, linked_doctypes[ref_doctype] is removed
-			if linked_doctypes.get(ref_doctype) and shared and row[linked_doctypes[ref_doctype]] in shared:
+			if (
+				linked_doctypes.get(ref_doctype) and shared and row.get(linked_doctypes[ref_doctype]) in shared
+			):
 				result.append(row)
 
 			elif has_match(
