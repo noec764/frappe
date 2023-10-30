68fe7e16b4f8e2c11b9a1d0dbba4262b2856a695
Shariq Ansari <sharique.rik@gmail.com>
2023-10-30 18:16:17 +0530

    fix: absolute remove tab button
    also reduced tab header height

--- frappe/public/js/form_builder/components/Sidebar.vue
+++ frappe/public/js/form_builder/components/Sidebar.vue
@@ -126,7 +126,9 @@ function resize(e) {
 }
 
 .default-state {
-	height: calc(100vh - 196px);
+	display: flex;
+	flex-direction: column;
+	height: calc(100vh - 166px);
 
 	.actions {
 		padding: 5px;
@@ -135,10 +137,10 @@ function resize(e) {
 		border-bottom: 1px solid var(--border-color);
 	}
 	.empty-state {
+		flex: 1;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		height: 100%;
 		text-align: center;
 		color: var(--disabled-text-color);
 	}
